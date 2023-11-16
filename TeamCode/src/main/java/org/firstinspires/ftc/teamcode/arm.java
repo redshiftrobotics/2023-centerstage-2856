c2cb898b6cb856f664d7ea8baf5e4f820a49289a
Aceius E <io@acei.us>
2023-11-15 16:38:27 -0800

    New naming convention for testopmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arm.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "Arm Tick Measurement")
+@TeleOp(name = "[Test] Arm Tick Measurement")
 public class arm extends LinearOpMode {
   private DcMotor Arm;
   
