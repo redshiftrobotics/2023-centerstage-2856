085ab2eeea5dd6f38a9fb1c8b241c38e038a6c40
Aceius E <io@acei.us>
2023-12-02 16:35:44 -0800

    Prototype OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueFarPixelPlacement.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import java.util.concurrent.TimeUnit;
+
+@Autonomous(name = "[Prototype] Blue Far")
+public class BlueFarPixelPlacement extends OdometryEnabledNavigator {
+    //private final OdometryEnabledNavigator navigator;
+    private final ElapsedTime runtime = new ElapsedTime();
+
+    private DcMotor arm;
+
+    @Override
+    public void runOpMode() {
+        arm = hardwareMap.get(DcMotor.class,"arm");
+        telemetry.addData("Status", "Initialized").setRetained(true);
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+        if (opModeIsActive()) {
+            telemetry.addData("Status", "Run Time: %s", runtime.toString());
+            // Drive to backdrop
+            driveLeftInches(29);
+            driveBackwardsInches(90);
+            // Drop pixel
+            // Target Position is -3306
+            while (opModeIsActive()) {
+                arm.setPower(-0.5);
+                arm.setTargetPosition(-3306);
+            }
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedFarPixelPlacement.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.concurrent.TimeUnit;
+
+@Autonomous(name = "[Prototype] Red Far")
+public class RedFarPixelPlacement extends OdometryEnabledNavigator {
+    //private final OdometryEnabledNavigator navigator;
+    private final ElapsedTime runtime = new ElapsedTime();
+
+    private DcMotor arm;
+
+    @Override
+    public void runOpMode() {
+        arm = hardwareMap.get(DcMotor.class,"arm");
+        telemetry.addData("Status", "Initialized").setRetained(true);
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+        if (opModeIsActive()) {
+            telemetry.addData("Status", "Run Time: %s", runtime.toString());
+            // Drive to backdrop
+            driveRightInches(29);
+            try {
+                /*if pirateTekerz*/ TimeUnit.SECONDS.sleep(15);
+            } catch (InterruptedException error) {
+                while (opModeIsActive()) {
+                    telemetry.addData("Error: ", error);
+                }
+                return;
+            }
+            driveBackwardsInches(90);
+            // Drop pixel
+            // Target Position is -3306
+            arm.setPower(-1);
+            arm.setTargetPosition(-3306);
+            while (opModeIsActive()) {
+                arm.setPower(-0.5);
+                arm.setTargetPosition(-3306);
+            }
+        }
+    }
+}
