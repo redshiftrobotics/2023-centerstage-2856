2371876830886ceb4dc28003f082757a4799cab7
Aceius E <io@acei.us>
2023-12-09 14:16:12 -0800

    Remove unused import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometryEnabledNavigator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometryEnabledNavigator.java
@@ -1,11 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 import java.util.concurrent.TimeUnit;
 
 public abstract class OdometryEnabledNavigator extends LinearOpMode {
